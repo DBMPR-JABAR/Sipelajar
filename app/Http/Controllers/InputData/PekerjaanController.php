6004b7a84a370b7824009acf6ea20ed708e85795
rival2905 <valdothea@gmail.com>
2021-10-06 13:53:58 +0700

    lat

--- app/Http/Controllers/InputData/PekerjaanController.php
+++ app/Http/Controllers/InputData/PekerjaanController.php
@@ -420,6 +420,8 @@ class PekerjaanController extends Controller
         
         $pekerjaan = $req->except(['_token','tanggal_awal','tanggal_akhir','uptd_filter']);
         // dd($pekerjaan);
+        if(strpos($req->lat, '-') == false) $pekerjaan['lat'] = '-'.$req->lat;
+
         $pekerjaan['uptd_id'] = $req->uptd_id == '' ? 0 : $req->uptd_id;
         if($pekerjaan['uptd_id'])
             $pekerjaan['uptd_id'] = str_replace('uptd', '', $pekerjaan['uptd_id']);
